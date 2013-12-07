2ea5c0c585e002c16623d88106c06346641a60bc
Chad Mills <chadmmills@me.com>
2013-12-07 10:52:05 -0500

    Updated login form remember me style

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,31 @@
 
 ActiveRecord::Schema.define(version: 20131103212438) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "admins", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  add_index "admins", ["unlock_token"], name: "index_admins_on_unlock_token", unique: true, using: :btree
+
   create_table "mileage_records", force: true do |t|
     t.date     "record_date"
     t.integer  "start_mileage"
@@ -25,7 +50,17 @@ ActiveRecord::Schema.define(version: 20131103212438) do
     t.datetime "updated_at"
   end
 
-  add_index "mileage_records", ["user_id"], name: "index_mileage_records_on_user_id"
+  add_index "mileage_records", ["user_id"], name: "index_mileage_records_on_user_id", using: :btree
+
+  create_table "shops", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "image_url"
+    t.string   "phone_number"
+    t.string   "web_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -42,7 +77,7 @@ ActiveRecord::Schema.define(version: 20131103212438) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
