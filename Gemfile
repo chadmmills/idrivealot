b43bc3203d0a4598236bb33a307a37c3143b9df3
Chad Mills <chadmmills@me.com>
2013-12-07 17:22:45 -0500

    Added download to xlsx functionality

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
+gem 'acts_as_xlsx'
 gem 'axlsx_rails', '~>0.1.5'
 
 group :production do
--- app/controllers/mileage_records_controller.rb
+++ app/controllers/mileage_records_controller.rb
@@ -9,6 +9,7 @@ class MileageRecordsController < ApplicationController
     respond_to do |f|
       f.html { render "table_index" }
       f.csv { send_data @mileage_records.to_csv(current_user), type: 'text/csv; charset=utf-8; header=present' }
+      f.xlsx { send_data @mileage_records.to_xlsx.to_stream.read, :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet" }
     end
   end
 
