32700742b08a82df2757866726d13229eba83801
Chad Mills <chadmmills@me.com>
2014-01-26 09:35:14 -0500

    Fixed .persisted? error on nil

--- app/controllers/mileage_records_controller.rb
+++ app/controllers/mileage_records_controller.rb
@@ -6,7 +6,7 @@ class MileageRecordsController < ApplicationController
   # GET /mileage-records
   def index
     @mileage_records = current_user.mileage_records.order("record_date DESC, end_mileage DESC")
-    @mileage_record = current_user.mileage_records.order(:record_date).last
+    @mileage_record = current_user.mileage_records.new
     respond_to do |f|
       f.html { render "table_index" }
       f.csv { send_data @mileage_records.to_csv(current_user), type: 'text/csv; charset=utf-8; header=present' }
