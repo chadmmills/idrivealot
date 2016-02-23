e14adb9c714fec6265581554d48c4262a5053fb9
Chad Mills <chad@whiteboard.is>
2016-02-23 05:53:12 -0600

    Change self.attr calls to read_attribute(). Clean up tabs / formatting.

--- app/models/mileage_record.rb
+++ app/models/mileage_record.rb
@@ -1,66 +1,70 @@
 class MileageRecord < ActiveRecord::Base
 
-    # create_table :mileage_records do |t|
-    #   t.date :record_date
-    #   t.integer :start_mileage
-    #   t.integer :end_mileage
-    #   t.string :route_description
-    #   t.integer :distance
-    #   t.text :notes
-    #   t.references :user, index: true
+  # create_table :mileage_records do |t|
+  #   t.date :record_date
+  #   t.integer :start_mileage
+  #   t.integer :end_mileage
+  #   t.string :route_description
+  #   t.integer :distance
+  #   t.text :notes
+  #   t.references :user, index: true
 
-    #   t.timestamps
-	
-	attr_accessor :new_route_description
+  #   t.timestamps
 
-  validates :user_id, :record_date, :route_description, presence: true
-	validates :start_mileage, numericality: { only_integer: true }, if: :start_mileage_present?
-	validates :end_mileage, numericality: { only_integer: true }, if: :end_mileage_present?
-  validate :start_less_than_end_mileage
   belongs_to :user
+
+  attr_accessor :new_route_description
+
+  validates :user, :record_date, :route_description, presence: true
+  validates :start_mileage, numericality: { only_integer: true }, if: :start_mileage_present?
+  validates :end_mileage, numericality: { only_integer: true }, if: :end_mileage_present?
+
+  validate :start_less_than_end_mileage
 
   before_save :set_distance
 
-	def record_date
-		self[:record_date] || Date.today
-	end
+  def record_date
+    read_attribute(:record_date) || Date.today
+  end
 
-  public
-    def self.last_end_mileage_for(user)
-      if MileageRecord.where(user_id: user).count < 1
-        return 0
-      else
-        MileageRecord.where(user_id: user).last.end_mileage
+  def self.last_end_mileage_for(user)
+    if MileageRecord.where(user: user).exists?
+      MileageRecord.where(user: user).last.end_mileage
+    else
+      0
+    end
+  end
+
+  def self.to_csv(user)
+    CSV.generate do |csv|
+      csv << column_names
+      where(user: user).each do |record|
+        csv << record.attributes.values_at(*column_names)
       end
     end
-
-    def self.to_csv(user)
-      CSV.generate do |csv|
-        csv << column_names
-        where(user_id: user).each do |record|
-          csv << record.attributes.values_at(*column_names)
-        end
-      end
-    end
+  end
 
   private
+
     def set_distance
-      self.distance = self.end_mileage - self.start_mileage if self.end_mileage
+      if read_attribute(:end_mileage).present?
+        self[:distance] = read_attribute(:end_mileage) - read_attribute(:start_mileage)
+      end
     end
 
     def start_less_than_end_mileage
-      unless self.end_mileage.nil?
-        if self.end_mileage < self.start_mileage ||= 0
+      unless end_mileage.blank?
+        if read_attribute(:end_mileage) < read_attribute(:start_mileage)
           errors[:base] << "End Mileage must be greater than Start Mileage"
         end
       end
     end
-		
-		def start_mileage_present?
-			self.start_mileage 
-		end
 
-		def end_mileage_present?
-			 self.end_mileage
-		end
+    def start_mileage_present?
+      start_mileage.present?
+    end
+
+    def end_mileage_present?
+      end_mileage.present?
+    end
 end
--- spec/models/mileage_record_spec.rb
+++ spec/models/mileage_record_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
     # create_table :mileage_records do |t|
     #   t.date :record_date
@@ -12,51 +12,68 @@ require 'spec_helper'
     #   t.timestamps
 
 describe MileageRecord do
-	it "properly sets the date" do
-		#with default value of Date.today, shouldn't be nil
-		#Instead should properly save mileage
+	it "properly sets the date with a value" do
 		mileage_entry = build(:mileage_record, record_date: Date.today - 5.days)
+
 		expect(mileage_entry.record_date).to eq(Date.today - 5.days)
   end
 
-  it " has an invalid route" do
-		expect(build(:mileage_record, route_description: nil)).to have(1).errors_on(:route_description)
+	it "properly sets the date given no value" do
+		mileage_entry = build(:mileage_record, record_date: nil)
+
+		expect(mileage_entry.record_date).to eq(Date.today)
+  end
+  
+  it "has a default record_date value" do
+    expect(MileageRecord.new.record_date).to_not be_nil
   end
 
-  it "has an invalid mileage value" do
-		expect(build(:mileage_record, start_mileage: 1.34)).to have(1).errors_on(:start_mileage)
+
+  describe "Validations" do
+    it " has an invalid route" do
+      expect(build(:mileage_record, route_description: nil)).to have(1).errors_on(:route_description)
+    end
+
+    it "has an invalid mileage value" do
+      expect(build(:mileage_record, start_mileage: 1.34)).to have(1).errors_on(:start_mileage)
+    end
+
+    it "has a valid mileage value" do
+      expect(build(:mileage_record, start_mileage: 1024)).to have(0).errors_on(:start_mileage)
+    end
+
+    it "has an invalid mileage value" do
+      expect(build(:mileage_record, end_mileage: 1.024)).to have(1).errors_on(:end_mileage)
+    end
+
+    it "is allowed to have nil mileage" do
+      expect(build(:mileage_record, end_mileage: nil, start_mileage: nil)).to be_valid
+    end
+
+    it "is allowed to have nil end mileage" do
+      expect(build(:mileage_record, end_mileage: nil, start_mileage: 90)).to be_valid
+    end
+
+    it "is a valid record" do
+      expect(create(:mileage_record)).to be_valid
+    end
+
+    it "saves the distance of the trip" do
+      record = create(:mileage_record, start_mileage: 5, end_mileage: 10)
+      expect(record.distance).to eq(5)
+    end
+
+    it "is invalid due to start_mileage > end_mileage" do
+      expect(build(:mileage_record, start_mileage: 15, end_mileage: 10)).to be_invalid
+    end
   end
 
-  it "has a valid mileage value" do
-		expect(build(:mileage_record, start_mileage: 1024)).to have(0).errors_on(:start_mileage)
-  end
+  describe "last_end_mileage_for" do
+    it 'should return 0 if first record for user' do
+      user = build(:user)
 
-  it "has an invalid mileage value" do
-		expect(build(:mileage_record, end_mileage: 1.024)).to have(1).errors_on(:end_mileage)
-  end
-
-	it "is allowed to have nil mileage" do
-		expect(build(:mileage_record, end_mileage: nil, start_mileage: nil)).to be_valid
-	end
-
-	it "is allowed to have nil end mileage" do
-		expect(build(:mileage_record, end_mileage: nil, start_mileage: 90)).to be_valid
-	end
-
-	it "has a default record_date value" do
-		expect(MileageRecord.new.record_date).to_not be_nil
-	end
-
-  it "is a valid record" do
-    expect(create(:mileage_record)).to be_valid
-  end
-
-  it "saves the distance of the trip" do
-    record = create(:mileage_record, start_mileage: 5, end_mileage: 10)
-    expect(record.distance).to eq(5)
-  end
-
-  it "is invalid due to start_mileage > end_mileage" do
-    expect(build(:mileage_record, start_mileage: 15, end_mileage: 10)).to be_invalid
+      expect(MileageRecord.last_end_mileage_for(user)).to eq 0
+    end
+    
   end
 end
