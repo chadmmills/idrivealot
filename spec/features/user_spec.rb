4bb5f04edc2337175c6048fcc988ff59a9f2d4ed
Chad Mills <chad@whiteboard.is>
2016-02-23 06:22:59 -0600

    Use StripeMock for user sign up spec instead of manually stub request

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 
 feature "User" do
-  scenario "can signup and pay with credit card", js: true do
-    stub_request(:post, /stripe.com/).to_return(status: 200, body: '{"id": "tok_u5dg20Gra"}', headers: {})
+  scenario "can signup and pay with credit card" do
+    stripe_helper = StripeMock.create_test_helper 
+    StripeMock.start 
+    stripe_helper.create_plan id: "idrivealot_monthly", name: "idrivealot_monthly"
+
     visit new_user_registration_path
     fill_in "Email", with: "email@example.com"
     fill_in "Password", with: "password123"
-
-    #save_and_open_page
+    find(:xpath, "//input[@id='user_stripe_card_token']").set stripe_helper.generate_card_token
     fill_in "card_number", with: "4242424242424242"
     fill_in "card_code", with: "123"
     select "12", from: "card_month"
     select Date.current.year + 1, from: "card_year"
-
     click_button "Sign Up"
 
     expect(page).to have_content "Welcome"
+
+    StripeMock.stop 
   end
 
   context "with existing plan" do
